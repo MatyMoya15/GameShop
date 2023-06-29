22ec9c756973fdcedc1447a8185e9663a237dc4a
MatyMoya15 <matymoyano4@gmail.com>
2023-06-29 01:46:51 -0300

    230629

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { GamesComponent } from './components/games/games.component';
+import { ComponentsComponent } from './components/components/components.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
+
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'games', component: GamesComponent
+  },
+  {
+    path:'components', component: ComponentsComponent
+  },
+  {
+    path:'404', component: NotFoundComponent
+  },
+  {
+    path:'soon', component: ComingSoonComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ComponentsComponent } from './components/components/components.component';
+import { GamesComponent } from './components/games/games.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ComponentsComponent,
+    GamesComponent,
+    NotFoundComponent,
+    ComingSoonComponent
   ],
   imports: [
     BrowserModule,
